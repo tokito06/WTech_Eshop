c602ecc019f3f7d9c84c0647833292e4e9b2e4a0
Vadim Popovych <zyzel19@gmail.com>
2026-04-13 13:52:15 +0200

    refactor(search): refactor product display to use reusable product card component

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -170,7 +170,6 @@
             <!-- Search items -->
             <div class="col-12 col-md-9 col-lg-10">
                 <div class="row g-3" id="products-grid">
-
                     @forelse($products as $product)
                         @php
                             $image = $product->images->first();
@@ -179,24 +178,7 @@
                             $price = $product->variants->min('price') ?? 0;
                         @endphp
                         <div class="col-6 col-lg-4 col-xxl-3">
-                            <div class="product-card">
-                                <button class="product-card__fav"><span class="material-symbols-outlined">favorite</span></button>
-                                <a href="{{ route('product', $product) }}" class="clear-link">
-                                    <div class="product-card__img">
-                                        <img src="{{ $imageUrl }}" alt="{{ $product->name }} image">
-                                        <div class="product-card__sizes">
-                                            @foreach($sizes as $size)
-                                                <span class="product-card__size-tag">{{ $size }}</span>
-                                            @endforeach
-                                        </div>
-                                    </div>
-                                    <div class="product-card__body">
-                                        <h3 class="product-card__name">{{ $product->name }}</h3>
-                                        <p class="product-card__desc">{{ \Illuminate\Support\Str::limit($product->description, 120) }}</p>
-                                        <span class="product-card__price">{{ number_format((float) $price, 2, ',', ' ') }} €</span>
-                                    </div>
-                                </a>
-                            </div>
+                            @include('components.product-card', ['product' => $product])
                         </div>
                     @empty
                         <div class="col-12">
