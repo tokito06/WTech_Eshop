1446b9056c9a9fa33429a1e6d992423e7af22366
Vadim Popovych <zyzel19@gmail.com>
2026-05-10 17:12:46 +0200

    feat: add middleware EnsureGuestToken

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'seller' => \App\Http\Middleware\SellerMiddleware::class,
         ]);
+        $middleware->web(append: [
+            \App\Http\Middleware\EnsureGuestToken::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
